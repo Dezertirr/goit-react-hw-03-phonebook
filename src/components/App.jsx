933d1d6fd8bb741614c23860377604d7bef390f0
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-05-30 17:03:02 +0300

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,100 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import shortid from "shortid";
+import { createForm } from "./Form/Form";
+import { FilterLabel } from "./Filter/Filter";
+import { PhoneBook } from "./phonebook/PhoneBook.jsx"; 
+
+
+export class App extends Component {
+  state = {
+    name: "",
+    number: "",
+    filter: "",
+    contacts: [],
+  };
+   componentDidMount(){
+    const contactsLocalStorage = JSON.parse(
+      localStorage.getItem('contacts')
+    );
+
+    if (contactsLocalStorage){
+      this.setState({contacts: contactsLocalStorage})
+    }
+   }
+
+   componentDidUpdate(prevProps, prevState){
+    const {contacts} = this.state;
+
+    if (
+      contacts.length !== prevState.contacts.length &&
+      contacts.length !== 0
+    ) {
+      localStorage.setItem ('contacts', JSON.stringify(contacts))
+    }
+   }
+
+  handleChange = (evt) => {
+    const { name, value } = evt.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    const { name, number, contacts } = this.state;
+    const existingContact = contacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if(existingContact) {
+      alert ('this contact already exists')
+      return;
+    }
+
+    const newContact = {
+      id: shortid.generate(),
+      name: name,
+      number: number,
+    };
+    
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+      name: "",
+      number: "",
+    }));
+  };
+
+  handleFilter = (filterValue) => {
+    this.setState({ filter: filterValue });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  handleDelete = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => {
+        return contact.id !== id;
+      }),
+    }));
+  };
+
+  render() {
+    const { name, number } = this.state;
+    const filteredContacts = this.getFilteredContacts();
+    
+
+    return (
+      <div>
+        {createForm(this.handleChange, this.handleSubmit, name, number)}
+        <FilterLabel handleFilter={this.handleFilter} />
+          <PhoneBook contacts={filteredContacts} handleDelete={this.handleDelete} />
+      </div>
+    );
+  }
+}
